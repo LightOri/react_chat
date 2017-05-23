789f7fc6caedcd07731a17eec60543f9fa6073a1
Andrii Vasylytsia <andrii.vasylytsia@gmail.com>
2017-05-23 09:52:05 +0300

    accumulated commit

--- src/components/Chat/MessagesPanel/Messages/your_message/index.js
+++ src/components/Chat/MessagesPanel/Messages/your_message/index.js
@@ -3,13 +3,25 @@ import './your_message.css';
 
 class YourMessage extends React.Component {
   render() {
-  	const message = JSON.stringify(this.props.message);
+        const message = this.props.message;
+        const formattedMessage = transformMessages(message);
+
+        function transformMessages(data) {
+            const messageData = data;
+            if (typeof messageData === 'string') {
+                return messageData;
+            } else if (messageData.msg){
+              return messageData.msg;
+            }else {
+                return "wrong message format"
+            }
+        }
 
     return (
 		<div className="message message--right">
 			<div className="message__icon round-user-icon"></div>
 			<div className="message__text message__text--right">
-				{message}
+				{formattedMessage}
 				<div className="message__time message__time--right">13:58</div>
 			</div>
 		</div>
